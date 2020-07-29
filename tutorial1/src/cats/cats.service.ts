5104d283fd92f85aeab1a18e3dac6179d9510983
Suranart Niamcome <suranartnc@gmail.com>
2020-07-29 17:58:16 +0700

    Adopt HTTP module

--- tutorial1/src/cats/cats.service.ts
+++ tutorial1/src/cats/cats.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+import { map } from 'rxjs/operators';
+
 import { Cat } from './interfaces/cat.interface';
 
 @Injectable()
 export class CatsService {
+  constructor(private httpService: HttpService) {}
+  
   private readonly cats: Cat[] = [];
 
   create(cat: Cat) {
     this.cats.push(cat);
   }
 
-  findAll(): Cat[] {
-    return this.cats;
+  findAll() {
+    const url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/07-28-2020.csv'
+    return this.httpService.get(url).pipe(map(response => response.data))
+    
   }
 }
