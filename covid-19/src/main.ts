8c1825676e61928a429c56071baa93621cf1eefd
Suranart Niamcome <suranartnc@gmail.com>
2020-07-30 18:21:48 +0700

    Enable compression

--- covid-19/src/main.ts
+++ covid-19/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import * as compression from 'compression';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.use(compression());
+
   await app.listen(configService.get('global.port'));
 }
 bootstrap();
