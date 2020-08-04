359b6e6c6622bc06038b5447cc584198094d6dbf
Suranart Niamcome <suranartnc@gmail.com>
2020-08-04 16:02:44 +0700

    Adopt module aliases

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import * as compression from 'compression';
 
-import { AppModule } from './modules/app/app.module';
+import { AppModule } from '@src/modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import globalConfig from './config/global.config';
-import databaseConfig from './config/database.config';
-import cacheConfig from './config/cache.config';
 
-import { CatsModule } from '../cats/cats.module';
+import globalConfig from '@src/modules/app/config/global.config';
+import databaseConfig from '@src/modules/app/config/database.config';
+import cacheConfig from '@src/modules/app/config/cache.config';
+
+import { CatsModule } from '@src/modules/cats/cats.module';
 
 @Module({
   imports: [
