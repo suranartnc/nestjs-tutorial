530ed54686d2e4cac20cb184ce03be4d7ac7bab7
Suranart Niamcome <suranartnc@gmail.com>
2020-08-04 16:06:47 +0700

    Change alias config

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,11 +2,11 @@ import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import globalConfig from '@src/modules/app/config/global.config';
-import databaseConfig from '@src/modules/app/config/database.config';
-import cacheConfig from '@src/modules/app/config/cache.config';
+import globalConfig from '@modules/app/config/global.config';
+import databaseConfig from '@modules/app/config/database.config';
+import cacheConfig from '@modules/app/config/cache.config';
 
-import { CatsModule } from '@src/modules/cats/cats.module';
+import { CatsModule } from '@modules/cats/cats.module';
 
 @Module({
   imports: [
